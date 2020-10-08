6523c02ab7bd93e002a165d0c10e837627139100
ASHUTOSH <ashutosh@192.168.1.3>
2020-10-08 14:29:36 +0530

    [Ashutosh]Added:Check Maximum Integer

--- src/main/java/com/generic/GenericMaximum.java
+++ src/main/java/com/generic/GenericMaximum.java
@@ -2,8 +2,21 @@ package com.generic;
 
 public class GenericMaximum {
 
+	public int maxInteger(Integer a,Integer b,Integer c)
+	{
+		Integer max = a;
+		if(b.compareTo(max)>0) {
+			max=b;
+		}
+		if(c.compareTo(max)>0) {
+			max=c;
+		}
+		return max;
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
 
 	}
 
--- src/test/java/com/generic/GenericMaximumTest.java
+++ src/test/java/com/generic/GenericMaximumTest.java
@@ -1,5 +1,37 @@
 package com.generic;
-
+import org.junit.*;
+import org.junit.Assert;
 public class GenericMaximumTest {
+	
+	@Test
+	public void givenFirst_Greater_ShouldReturnFirst() {
+		GenericMaximum obj = new GenericMaximum();
+		Integer result = obj.maxInteger(30, 20, 10);
+		boolean check=false; 
+		if(result == 100) {
+		check = true;
+		} 
+		Assert.assertEquals(true, check);;
+	}
+	@Test
+	public void givenSecond_Greater_ShouldReturnSecond() {
+		GenericMaximum obj = new GenericMaximum();
+		Integer result = obj.maxInteger(20, 30, 10);
+		boolean check=false; 
+		if(result == 100) {
+		check = true;
+		} 
+		Assert.assertEquals(true, check);;
+	}
+	@Test
+	public void givenThird_Greater_ShouldReturnThird() {
+		GenericMaximum obj = new GenericMaximum();
+		Integer result = obj.maxInteger(20, 10, 30);
+		boolean check=false; 
+		if(result == 100) {
+		check = true;
+		} 
+		Assert.assertEquals(true, check);;
+	}
 
 }
