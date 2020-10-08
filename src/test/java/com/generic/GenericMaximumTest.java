7e75432b0fbbdc9707543a157e7dc6f7b51ebab9
ASHUTOSH <ashutosh@192.168.1.3>
2020-10-08 14:36:12 +0530

    [Ashutosh]Added:Check Maximum Float

--- src/test/java/com/generic/GenericMaximumTest.java
+++ src/test/java/com/generic/GenericMaximumTest.java
@@ -8,7 +8,7 @@ public class GenericMaximumTest {
 		GenericMaximum obj = new GenericMaximum();
 		Integer result = obj.maxInteger(30, 20, 10);
 		boolean check=false; 
-		if(result == 100) {
+		if(result == 30) {
 		check = true;
 		} 
 		Assert.assertEquals(true, check);;
@@ -18,7 +18,7 @@ public class GenericMaximumTest {
 		GenericMaximum obj = new GenericMaximum();
 		Integer result = obj.maxInteger(20, 30, 10);
 		boolean check=false; 
-		if(result == 100) {
+		if(result == 30) {
 		check = true;
 		} 
 		Assert.assertEquals(true, check);;
@@ -28,10 +28,41 @@ public class GenericMaximumTest {
 		GenericMaximum obj = new GenericMaximum();
 		Integer result = obj.maxInteger(20, 10, 30);
 		boolean check=false; 
-		if(result == 100) {
+		if(result == 30) {
 		check = true;
 		} 
 		Assert.assertEquals(true, check);;
 	}
+	@Test
+	public void givenFirst_Greater_ShouldReturnFirst_Float() {
+		GenericMaximum obj = new GenericMaximum();
+		Float result = obj.maxFloat(30f, 20f, 10f);
+		boolean check=false; 
+		if(result == 30f) {
+		check = true;
+		} 
+		Assert.assertEquals(true, check);;
+	}
+	@Test
+	public void givenSecond_Greater_ShouldReturnSecond_Float() {
+		GenericMaximum obj = new GenericMaximum();
+		Float result = obj.maxFloat(20f, 30f, 10f);
+		boolean check=false; 
+		if(result == 30f) {
+		check = true;
+		} 
+		Assert.assertEquals(true, check);;
+	}
+	@Test
+	public void givenThird_Greater_ShouldReturnThird_Float() {
+		GenericMaximum obj = new GenericMaximum();
+		Float result = obj.maxFloat(20f, 10f, 30f);
+		boolean check=false; 
+		if(result == 30f) {
+		check = true;
+		} 
+		Assert.assertEquals(true, check);;
+	}
+
 
 }
